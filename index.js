57a3fd4be3b5f7c193c32fda74ffa1126c99f2e8
Grégoire Audoux <projects@gregoire-auodux.fr>
2018-10-24 13:56:45 +0200

    wip
    work in progress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import '///dev.jspm.io/angular';
+import '///dev.jspm.io/@uirouter/angularjs'
 
 import {main} from '/components/main/main.js'
 
-export const rootModule = angular.module('root', [main.name]);
+export const rootModule = angular.module('root', ['ui.router', main.name]);
