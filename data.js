b8ff6cd57bf56f018e9c7e7156d13212fdecceb2
Grégoire Audoux <project@gregoire-audoux.fr>
2018-11-06 13:42:11 +0100

    update data.js

--- data.js
+++ data.js
@@ -16,7 +16,165 @@ export const data = [
             '25 BH à 5m, pied de pivot derrière plot à une coudée',
             '25 FH à 5m, pied de pivot derrière plot à une coudée',
             '25 BH à 10m, pied de pivot derrière plot à une coudée',
-            '25 FH à 10m, pied de pivot derrière plot à une coudée'
+            '25 FH à 10m, pied de pivot derrière plot à une coudée',
+        ]
+    },
+    {
+        title: 'Disque orange : fondamentaux avec distance plus importante',
+        objective: .9,
+        data: [
+            '20 BH à 10m, pied de pivot derrière plot à une coudée',
+            '10 BH à 15m, pied de pivot derrière plot à une coudée',
+            '10 BH à 20m, pied de pivot derrière plot à une coudée',
+            '20 FH à 10m, pied de pivot derrière plot à une coudée',
+            '10 FH à 15m, pied de pivot derrière plot à une coudée',
+            '10 FH à 20m, pied de pivot derrière plot à une coudée',
+            '10 hammers à 10m',
+            '10 scoobers à 10m'
+        ]
+    },
+    {
+        title: 'Disque rose : fondamentaux avec distance plus importante et taux de réussite élevé',
+        objective: .95,
+        data: [
+            '10 BH à 10m, pied de pivot derrière plot à une coudée',
+            '10 BH à 10m, feinte avec changement de pied de pivot derrière plots à une coudée',
+            '10 BH à 15m, pied de pivot derrière plot à une coudée',
+            '10 BH à 20m, pied de pivot derrière plot à une coudée',
+            '10 FH à 10m, pied de pivot derrière plot à une coudée',
+            '10 FH à 10m, feinte avec changement de pied de pivot derrière plots à une coudée',
+            '10 FH à 15m, pied de pivot derrière plot à une coudée',
+            '10 FH à 20m, pied de pivot derrière plot à une coudée',
+            '10 hammers à 15m',
+            '10 scoobers à 15m',
+        ]
+    },
+    {
+        title: "Disque vert clair : s'écarter du pied de pivot",
+        objective: .9,
+        data: [
+            '10 BH à 10m, feinte avec changement de pied de pivot derrière plots à une coudée',
+            '10 BH à 10m, pied de pivot derrière plot à deux coudées',
+            '10 BH à 10m, pied de pivot derrière plot à trois coudées',
+            '10 FH à 10m, feinte avec changement de pied de pivot derrière plots à une coudée',
+            '10 FH à 10m, pied de pivot derrière plot à deux coudées',
+            '10 FH à 10m, pied de pivot derrière plot à trois coudées',
+            '10 BH à 15m, pied de pivot derrière plot à deux coudées',
+            '10 FH à 15m, pied de pivot derrière plot à deux coudée',
+            '10 hammers à 15m',
+            '10 scoobers à 15m'
+        ]
+    },
+    {
+        title: "Disque vert foncé: renforcer l'écartement du pied de pivot + lancer bas",
+        objective: .95,
+        data: [
+            '10 BH à 10m, feinte avec changement de pied de pivot derrière plot à une coudée',
+            '10 BH à 10m, pied de pivot derrière plot à trois coudées avec feinte une coudée',
+            '10 FH à 10m, feinte avec changement de pied de pivot derrière plot à une coudée',
+            '10 FH à 10m, pied de pivot derrière plot à trois coudées avec feinte un coudée',
+            '10 BH à 10m, lancer le plus bas possible (minimum sous le genou)',
+            '10 FH à 10m, lancer le plus bas possible (minimum sous le genou)',
+            '10 BH à 20m, pied de pivot derrière plot à trois coudées',
+            '10 FH à 20m, pied de pivot derrière plot à trois coudée',
+            '10 hammers à 15m',
+            '10 scoobers à 15m'
+        ]
+    },
+    {
+        title: "Disque bleu clair: release loin, bas et haut",
+        objective: .95,
+        data: [
+            '10 BH à 10m, pied de pivot derrière plot à trois coudées + feinte deux coudées',
+            '10 BH à 10m, lancer le plus bas possible (minimum sous le genou)',
+            '10 FH à 10m, pied de pivot derrière plot à trois coudées + feinte deux coudées',
+            '10 FH à 10m, lancer le plus bas possible (minimum sous le genou)',
+            '10 BH à 20m, lancer le plus bas possible (minimum sous le genou)',
+            '10 FH à 20m, lancer le plus bas possible (minimum sous le genou)',
+            '10 BH à 10m, lancer le plus haut possible (minimum haut dessus de la ligne des épaules)',
+            '10 FH à 10m, lancer le plus haut possible (minimum haut dessus de la ligne des épaules)',
+            '10 hammers à 20m',
+            '10 scoobers à 20m',
+        ]
+    },
+    {
+        title: "Disque bleu foncé: release loin, bas et haut avec précision",
+        objective: .95,
+        data: [
+            '10 BH à 20m, pied de pivot derrière plot à deux coudées + feinte',
+            '10 FH à 20m, pied de pivot derrière plot à deux coudées + feinte',
+            '10 BH à 20m, lancer le plus bas possible (minimum sous le genou) + feinte',
+            '10 FH à 20m, lancer le plus bas possible (minimum sous le genou) + feinte',
+            '10 BH à 20m, lancer le plus haut possible (minimum haut dessus de la ligne des épaules) + feinte',
+            '10 FH à 20m, lancer le plus haut possible (minimum haut dessus de la ligne des épaules) + feinte',
+            '10 BH à 30m, pied de pivot libre + feinte',
+            '10 FH à 30m, pied de pivot libre + feinte',
+            '10 hammers à 30m',
+            '10 scoobers à 30m',
+        ]
+    },
+    {
+        title: 'Disque violet : lancers alternatifs',
+        objective: .95,
+        data: [
+            '10 BH à 10m, sans pied de pivot, face au lanceur',
+            '10 FH à 10m, sans pied de pivot, face au lancer',
+            '10 BH main gauche à 10m, sans pied de pivot, face au lanceur',
+            '10 BH à 20m, sans pied de pivot, face au lanceur + feinte',
+            '10 FH à 20m, sans pied de pivot, face au lancer + feinte',
+            '10 BH main gauche à 20m, sans pied de pivot, face au lanceur',
+            '10 BH horloge',
+            '10 FH horloge',
+            '10 BH à 30m, pied de pivot libre',
+            '10 FH à 30m, pied de pivot libre'
+        ]
+    },
+    {
+        title: "Disque rouge : lancer alternatifs et dans l'espace",
+        objective: .95,
+        data: [
+            '10 BH à 20m, sans pied de pivot, face au lanceur',
+            '10 FH à 20m, sans pied de pivot, face au lancer',
+            '10 BH main gauche à 20m, sans pied de pivot, face au lanceur',
+            '10 BH démarrage de cut',
+            '10 FH démarrage de cut',
+            '10 BH outside in, derrière le partenaire, distance aléatoire',
+            '10 FH outside in, derrière le partenaire, distance aléatoire',
+            '10 BH inside out, à 2m du partenaire à 10m',
+            '10 FH inside out, à 2m du partenaire à 10m',
+            '10 BH low release entre les jambes à 10 m'
+        ]
+    },
+    {
+        title: 'Disque marron : overall avec vitesse, précision et distance',
+        objective: .95,
+        data: [
+            '10 BH à 10m, pied de pivot avec feinte derrière plot à trois coudées, release rapide',
+            '10 FH à 10m, pied de pivot avec feinte derrière plot à trois coudées, release rapide',
+            '10 BH à 10m low release + feinte derrière plot à deux coudées, release rapide',
+            '10 FH à 10m low release + feinte derrière plot à deux coudées, release rapide',
+            '10 BH outside in, derrière le partenaire, distance aléatoire',
+            '10 FH outside in, derrière le partenaire, distance aléatoire',
+            '10 BH inside out, à 2m du partenaire à 10m',
+            '10 FH inside out, à 2m du partenaire à 10m',
+            '10 BH break arround à 4m du partenaire à 10m',
+            '10 FH break arround à 4m du partenaire à 10m'
+        ]
+    },
+    {
+        title: 'Disque noir : overall avec vitesse, précision et distance, 100% de réussite',
+        objective: 1,
+        data: [
+            '10 BH à 10m, pied de pivot avec feinte derrière plot à trois coudées, release rapide',
+            '10 FH à 10m, pied de pivot avec feinte derrière plot à trois coudées, release rapide',
+            '10 BH à 10m low release + feinte derrière plot à deux coudées, release rapide',
+            '10 FH à 10m low release + feinte derrière plot à deux coudées, release rapide',
+            '10 BH outside in, derrière le partenaire, distance aléatoire',
+            '10 FH outside in, derrière le partenaire, distance aléatoire',
+            '10 BH inside out, à 2m du partenaire à 10m',
+            '10 FH inside out, à 2m du partenaire à 10m',
+            '10 BH break arround à 4m du partenaire à 10m',
+            '10 FH break arround à 4m du partenaire à 10m',
         ]
     }
 ]
