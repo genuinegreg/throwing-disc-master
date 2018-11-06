02b600b53c547743164dea608551265d0a8212f6
Grégoire Audoux <project@gregoire-audoux.fr>
2018-11-06 13:51:03 +0100

    Angular-material init ok

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import '///dev.jspm.io/angular';
 import '///dev.jspm.io/@uirouter/angularjs'
+import ngMaterial from '///dev.jspm.io/angular-material'
+import ngAnimate from '///dev.jspm.io/angular-animate'
+import ngAria from '///dev.jspm.io/angular-aria'
+import ngMessages from '///dev.jspm.io/angular-messages'
 
 import debug from '///dev.jspm.io/debug';
 
 const log = debug('tdm');
+log('initializing...', ngMaterial)
 
 import {main} from '/components/main/main.js'
 
-export const rootModule = angular.module('root', ['ui.router', main.name]);
+export const rootModule = angular.module('root', [
+    'ui.router',
+    ngMaterial,
+    ngAnimate,
+    ngAria,
+    ngMessages,
+    main.name
+]);
 
 log('main module initialized')
